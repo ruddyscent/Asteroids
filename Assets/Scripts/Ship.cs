2d713fc6dbe6a421ef3a04f17795f7813768c6f9
Kyungwon Chun <ruddyscent@gmail.com>
2020-07-24 14:31:45 +0900

    Step 3 of shipbuilding 101

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    // private Rigidbody2D rigidbody2D;
+    private Vector2 thrustDirection;
+    private float ThrustForce = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rigidbody2D = GetComponent<Rigidbody2D>();
+        thrustDirection = new Vector2(1.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+
+        if (Input.GetAxis("Thrust") > 0)
+        {
+            GetComponent<Rigidbody2D>().AddForce(ThrustForce * thrustDirection);
+        }
+    }
+}
