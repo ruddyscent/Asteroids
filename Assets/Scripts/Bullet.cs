688d562a151264130d8debef2a28a07cc3b9313f
Kyungwon Chun <ruddyscent@gmail.com>
2020-08-12 16:53:11 +0900

    Complete project for week 3

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    float bulletLifeTime = 2f;
+    Timer bulletLifeTimer;
+    GameObject thisBullet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisBullet = GetComponent<GameObject>();
+        GameObject[] ship = GameObject.FindGameObjectsWithTag("Ship");
+        foreach (GameObject obj in ship)
+        {
+            Physics2D.IgnoreCollision(obj.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+
+        GameObject[] otherBullet = GameObject.FindGameObjectsWithTag("Bullet");
+        foreach (GameObject obj in ship)
+        {
+            Physics2D.IgnoreCollision(obj.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+
+        // start spawn timer
+        bulletLifeTimer = gameObject.AddComponent<Timer>();
+        bulletLifeTimer.Duration = bulletLifeTime;
+        bulletLifeTimer.Run();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (bulletLifeTimer.Finished)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D coll)
